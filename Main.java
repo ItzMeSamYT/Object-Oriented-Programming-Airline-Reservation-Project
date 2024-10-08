fac412049b659fca9355ada5d8ed1ea41fb23ae3
Neutron <128963956+KIRAN-GITHUB123@users.noreply.github.com>
2024-10-08 19:58:48 +0530

    Update Main.java
    error marked

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
-import com.users.*;
 import com.catering.*;
 import com.dutyfree.*;
-import com.flightmanagement.*;
 import com.exceptions.*;
+import com.flightmanagement.*;
+import com.users.*;
+import java.util.Scanner;
 
 @SuppressWarnings("resource")
 class AirlineReservationSystem {
@@ -12,17 +12,30 @@ class AirlineReservationSystem {
         FlightSchedule flightSchedule = new FlightSchedule();
         DutyFreeManagement dutyFreeManagement = new DutyFreeManagement();
         CateringMenuManagement cateringMenuManagement = new CateringMenuManagement();
+        FlightReport flightReport = new FlightReport(flightSchedule);
 
         while (true) {
-            System.out.println("1. Manager Login");
-            System.out.println("2. Traveler Login");
+            System.out.println("1. Traveler Login");
+            System.out.println("2. Manager Login");
             System.out.println("3. Exit");
             System.out.print("Choose an option: ");
             int choice = scanner.nextInt();
             scanner.nextLine();  // Consume newline
 
             switch (choice) {
+
                 case 1:
+                    System.out.print("Enter Traveler Username: ");
+                    String travelerUsername = scanner.nextLine();
+                    System.out.print("Enter Traveler Password: ");
+                    String travelerPassword = scanner.nextLine();
+                    Traveler traveler = new Traveler(travelerUsername, travelerPassword, flightSchedule);
+                    try {
+                        traveler.t.join();
+                    } catch (InterruptedException e) {}
+                    break;
+
+                case 2:
                     System.out.print("Enter Manager Username: ");
                     String managerUsername = scanner.nextLine();
                     System.out.print("Enter Manager Password: ");
@@ -36,7 +49,7 @@ class AirlineReservationSystem {
 
                         boolean managerMenu = true;
                         while (managerMenu) {
-                            System.out.println("\n1. Manage Flights");
+                            System.out.println("1. Manage Flights");
                             System.out.println("2. Manage Duty-Free");
                             System.out.println("3. Manage Catering");
                             System.out.println("4. Logout");
@@ -48,37 +61,73 @@ class AirlineReservationSystem {
                                 case 1:
                                     boolean flightManagement = true;
                                     while (flightManagement) {
-                                        System.out.println("\n1. Add Flight");
-                                        System.out.println("2. Update Flight");
-                                        System.out.println("3. View Flights");
-                                        System.out.println("4. Delete Flight");
-                                        System.out.println("5. Back to Manager Menu");
+                                        System.out.println("\n1. Flight Report");
+                                        System.out.println("2. Add Flight");
+                                        System.out.println("3. Update Flight");
+                                        System.out.println("4. View Flights");
+                                        System.out.println("5. Delete Flight");
+                                        System.out.println("6. Back to Manager Menu");
                                         System.out.print("Choose an option: ");
                                         int flightManagementChoice = scanner.nextInt();
                                         System.out.println();
 
                                         switch (flightManagementChoice) {
                                             case 1:
+                                                boolean flightreport = true;
+                                                while (flightreport) {
+                                                    System.out.println("\n1. Flight Report");
+                                                    System.out.println("2. Full Capacity Flights");
+                                                    System.out.println("3. Frequent Booking Periods");
+                                                    System.out.println("4. Frequent Destinations");
+                                                    System.out.println("5. Back to Manager Menu");
+                                                    System.out.print("Choose an option: ");
+                                                    int flightReportChoice = scanner.nextInt();
+                                                    System.out.println();
+                                                    switch (flightReportChoice) {
+                                                        case 1:
+                                                            flightReport.generateReport();
+                                                            break;
+                                                        case 2:
+                                                            flightReport.reportFlightsWithCompleteCapacity();
+                                                            break;
+                                                        case 3:
+                                                            flightReport.reportFrequentBookingPeriods(traveler.bookFlight.bookingDates); //error
+                                                            break;
+                                                        case 4:
+                                                            flightReport.reportMostFrequentedDestinations();
+                                                            break;
+                                                        case 5:
+                                                            flightreport = false;
+                                                        default:
+                                                        try {
+                                                            throw new InvalidChoiceException("Invalid choice. Please try again.");
+                                                        } catch (InvalidChoiceException e) {
+                                                            System.out.println(e.getMessage());
+                                                        }
+                                                    }
+                                                }
+
+                                            case 2:
                                                 try {
                                                     manager.addFlight(flightSchedule);
                                                 } catch (ExceededMaxSizeException e) {
                                                     System.out.println(e.getMessage());
                                                 }
                                                 break;
-                                            case 2:
+                                            case 3:
                                                 try {
                                                     manager.updateFlight(flightSchedule);
                                                 } catch (InvalidChoiceException e) {
                                                     System.out.println(e.getMessage());
                                                 }
                                                 break;
-                                            case 3:
+                                            case 4:
                                                 flightSchedule.viewFlights();
                                                 break;
-                                            case 4:
+                                            case 5:
                                                 manager.deleteFlight(flightSchedule);
                                                 break;
-                                            case 5:
+                                            case 6:
                                                 flightManagement = false;
                                                 break;
                                             default:
@@ -118,17 +167,6 @@ class AirlineReservationSystem {
                     } else {
                         System.out.println("Invalid Manager Username or Password. Try again.");
                     }
-                    break;
-
-                case 2:
-                    System.out.print("Enter Traveler Username: ");
-                    String travelerUsername = scanner.nextLine();
-                    System.out.print("Enter Traveler Password: ");
-                    String travelerPassword = scanner.nextLine();
-                    Traveler traveler = new Traveler(travelerUsername, travelerPassword, flightSchedule);
-                    try {
-                        traveler.t.join();
-                    } catch (InterruptedException e) {}
                     break;
 
                 case 3:
